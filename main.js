da194fab146d7dd9bcb4b78db554a172f2c3400f
VanHungNguyen <vanhungnguyenbku@gmail.com>
2021-03-08 17:48:34 +0700

    masonry library

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ let collapse = document.querySelectorAll("#header .collapse");
 toggle.addEventListener('click', function() {
     collapse.forEach(col => col.classList.toggle("collapse-toggle"));
 });
+
+// with masonry
+var grid = document.querySelector('#posts .grid');
+var msnry = new Masonry(grid, {
+    // options...
+    itemSelector: '.grid-item',
+    columnWidth: 20
+});
